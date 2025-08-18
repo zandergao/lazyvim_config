526b69d9bd9537e742651b1e78db2675295b5e4d
gaozhan <gaozhan@seirobotics.net>
2025-08-18 14:35:39 +0800

    feat: 支持复制/剪切到系统剪贴板
    Normal 模式下：
    - yy → 复制整行到系统剪贴板
    - dd → 剪切整行到系统剪贴板
    Visual 模式下：
    - y  → 复制选中内容到系统剪贴板
    - d  → 剪切选中内容到系统剪贴板
    设置 vim.g.clipboard = 'osc52'，支持远程终端通过 OSC52 协议访问系统剪贴板
    
    Signed-off-by: gaozhan <gaozhan@seirobotics.net>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- 复制剪切到系统剪贴板
+vim.keymap.set('n', 'yy', '"+yy', { noremap = true})
+vim.keymap.set('n', 'dd', '"+dd', { noremap = true})
+vim.keymap.set('v', 'y', '"+y', { noremap = true })
+vim.keymap.set('v', 'd', '"+d', { noremap = true })
+
+vim.g.clipboard = 'osc52'
+
