2da83634820a0fddbb6784b0b76b86e7ef23588f
gaozhan <gaozhan@seirobotics.net>
2025-08-19 14:16:18 +0800

    add yanky and cursor ui
    Co-authored-by: gaozhan <gaozhan@seirobotics.net>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,9 +3,9 @@
 -- Add any additional options here
 -- 复制剪切到系统剪贴板
 vim.keymap.set('n', 'yy', '"+yy', { noremap = true})
-vim.keymap.set('n', 'dd', '"+dd', { noremap = true})
+-- vim.keymap.set('n', 'dd', '"+dd', { noremap = true})
 vim.keymap.set('v', 'y', '"+y', { noremap = true })
-vim.keymap.set('v', 'd', '"+d', { noremap = true })
+-- vim.keymap.set('v', 'd', '"+d', { noremap = true })
 
 vim.g.clipboard = 'osc52'
 
